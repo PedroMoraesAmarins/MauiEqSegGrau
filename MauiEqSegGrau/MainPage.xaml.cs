45883f1ac59797845c2f02195425fb7182de6c3c
Usuario <Usuario@CPD03-M13>
2024-02-19 12:25:21 -0300

    Update AppEqSegGrau

--- MauiEqSegGrau/MainPage.xaml.cs
+++ MauiEqSegGrau/MainPage.xaml.cs
@@ -1,24 +1,30 @@
-﻿namespace MauiEqSegGrau
+﻿using Microsoft.UI.Xaml.Controls.Primitives;
+
+namespace MauiEqSegGrau
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        double delta = 0;
+        double x1 = 0, x2 = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            double a = Convert.ToDouble(txt_a.Text);
+            double b = Convert.ToDouble(txt_b.Text);
+            double c = Convert.ToDouble(txt_c.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            delta = Math.Pow(b,2) - 4 * a * c;
+            x1 = (((-1) * b) + Math.Pow(delta, 0.5)) / (2 * a);
+            x2 = (((-1) * b) - Math.Pow(delta, 0.5)) / (2 * a);
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            string msg = $"O resultado é {x1} e {x2}";
+
+            DisplayAlert("Resultado", msg, "Fechar");
         }
     }
 
